8d0e294ede6fcfba095c175cf343b89815e50d2b
Dr. Hazem Ali <y3m@hotmail.com>
2020-06-08 21:35:44 +0200

    Copyrights Update

--- Core/Global.php
+++ Core/Global.php
@@ -4,7 +4,7 @@
  * @category   Web Development ( Programming )
  * @package    Skytells PHP Framework
  * @version    3.9
- * @copyright  2007-2018 Skytells, Inc. All rights reserved.
+ * @copyright  2007-2020 Skytells, Inc. All rights reserved.
  * @license    MIT | https://www.skytells.net/us/terms .
  * @author     Dr. Hazem Ali ( fb.com/Haz4m )
  * @see        The Framework's changelog to be always up to date.
